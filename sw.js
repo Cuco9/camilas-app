b76144aff919f6ea7e71c65a8a425b300498496d
Cuco9 <rogeralbertoperezguevara@gmail.com>
2026-06-02 14:06:24 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'camilas-v1';
+const ARCHIVOS = [
+  './',
+  './index.html',
+  'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.31.0/dist/tabler-icons.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ARCHIVOS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() =>
+      caches.match('./index.html')
+    ))
+  );
+});
