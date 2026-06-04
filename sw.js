36c7e2514dce8159bec08c017ac62ad5903d5b76
Cuco9 <rogeralbertoperezguevara@gmail.com>
2026-06-04 14:39:31 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Versión única basada en fecha/hora — cambia automáticamente con cada deploy
-const CACHE = 'camilas-' + '20260604-6';
+const CACHE = 'camilas-' + '20260604-7';
 const ARCHIVOS = [
   './',
   './index.html',
